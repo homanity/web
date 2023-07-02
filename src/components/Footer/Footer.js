79f1ca2817c85baba910fc7f17a1dc0a088e815e
MAYANK SINGH <113724942+mayank-singh-rajput@users.noreply.github.com>
2023-07-03 03:01:38 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-//import { faLinkedIn } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedIn } from "@fortawesome/free-solid-svg-icons";
 
 import linkedin from '../../images/socialIcons/linkedin2.jpg'
 import whatsapp from '../../images/socialIcons/whatsapp.jpg'
-//import fullLogo from '../../images/logo/finonicFull.png'
-import longLogo from '../../images/logo/findonicLogo2.png'
 
 function Footer() {
 
@@ -22,11 +20,11 @@ function Footer() {
             <div>
                 <div class="w-layout-grid footer-main-grid">
                     <div class="footer-left-wrapper">
-                        <a href="#" class="footer-logo-link w-nav-brand">
+                        {/* <a href="#" class="footer-logo-link w-nav-brand">
                             <img src={longLogo} loading="lazy" width="100" alt=""/>
-                        </a>
+                        </a> */}
                         <p class="subhead third">
-                        Homenity - <span class="gray-text">One stop Solution for building or renovating their homes.</span>
+                        Homanity - <span class="gray-text">One stop Solution for best electrical and plumbing services.</span>
                         </p>
                         <div class="margin-20px make-15px">
                             <p class="copyright">
@@ -53,31 +51,15 @@ function Footer() {
                                 <div>Contact</div>
                             </Link>
                         </div>
-                        <div class="foorer-list">
-                            <div class="footer-title">Resources</div>
-                            <a href="/news" class="footer-link w-inline-block">
-                                <div>Privacy Policy</div>
-                            </a>
-                            <a href="/template-info/style-guide" class="footer-link w-inline-block">
-                                <div>Term & Condition</div>
-                            </a>
-                            <a href="/template-info/licensing" class="footer-link w-inline-block">
-                                <div>User Aggrement</div>
-                            </a>
-                           
-                        </div>
+                        
                         <div class="foorer-list">
                             <div class="footer-title">Social</div>
                             <div class="social-wrapper left">
-                                <a href="https://www.instagram.com/_homanity/" target="_blank" rel='noreferrer' class="social-icon-footer w-inline-block">
+                                <a href="https://www.instagram.com/_homanity/" target="_blank" class="social-icon-footer w-inline-block">
                                     <img src="https://assets.website-files.com/62f12c77027bfb4b634fc1b9/62f12c77027bfbbf034fc1f7_instagram.svg" loading="lazy" width="20" alt=""/>
                                 </a>
-                               
-                                <a href="https://www.linkedin.com/company/findonic/" target="_blank" rel='noreferrer' class="social-icon-footer w-inline-block">
-                                    {/* <FontAwesomeIcon icon={faLinkedIn} /> */}
-                                    <img src={linkedin}  width="22" alt=""/>
-                                </a>
-                                <a href="https://wa.me/+919098630635" target="_blank" rel='noreferrer' class="social-icon-footer w-inline-block">
+
+                                <a href="https://wa.me/+919098630635" target="_blank" class="social-icon-footer w-inline-block">
                                     <img src={whatsapp} width="22" alt=""/>
                                 </a>
                             </div>
